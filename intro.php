62e3786da6232c9dcc10c7eb1ab75497dfc76569
abigael17 <ndegwaa74@gmail.com>
2021-06-17 11:04:33 +0300

    initial commit

---
+++ intro.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>introduction_to_php</title>
+</head>
+<body>
+
+<?php
+echo "hello safari lab members";
+
+
+?>
+
+
+</body>
+</html>
